8bd1c7d6a76952d9dac7c9b9f3fe42345306324f
kaiozumba93 <137240878+kaiozumba93@users.noreply.github.com>
2023-06-21 22:13:28 -0300

    Add files via upload

---
+++ os_cadastrar.php
@@ -0,0 +1,20 @@
+<?php
+include_once('conexao.php');
+if($_POST){
+    $idfuncos = $_POST['id_func_os'];
+    $idsesmet = $_POST['id_sesmt_os'];
+    $atividadeos = $_POST['atividade_os'];
+    $nros = $_POST['nr_os'];
+    $descos = $_POST['desc_os'];
+    $localpet = $_POST['local_pet'];
+    $espacopet = $_POST['espaco_pet'];
+    $dataemisaopet = $_POST['data_emissao_pet'];
+    $dataterminopet = $_POST['data_termino_pet'];
+    $acaopet = $_POST['acao_pet'];
+    $autorizadopet = $_POST['autorizado_pet'];
+    $vigiapet = $_POST['vigia_pet'];
+    $resgatepet = $_POST['resfare.pet']
+
+}
+
+?>
