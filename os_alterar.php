df28a0373732e88b094879d7dd60b3cfee9219d6
kaiozumba93 <137240878+kaiozumba93@users.noreply.github.com>
2023-06-30 22:14:43 -0300

    Add files via upload

---
+++ os_alterar.php
@@ -0,0 +1,33 @@
+<?php
+include_once('conexao.php');
+if($_POST){
+    $idos = $_POST['id_os'];
+    $idfuncos = $_POST['id_func_os'];
+    $idsesmet = $_POST['id_sesmt_os'];
+    $atividadeos = $_POST['atividade_os'];
+    $nros = $_POST['nr_os'];
+    $descos = $_POST['desc_os'];
+    $obsos = $_POST['obs_os'];
+    $statusos = $_POST['status_os'];
+
+  try {
+    $sql = $conn->prepare("
+         update OS set
+         Id_Funcionario_OrdemServico=:idfuncos,
+         Id_Sesmet_OrdemServico=:idsesmet,
+         nome_OrdemServico=:atividadeos, 
+        Id_Nr=:nros,
+        descricao_OrdemServico=:descos, 
+        obs_OrdemServico =:obsos,
+        status_OrdemServico=:statusos 
+        where Id_OrdemServico=:id_os
+
+  ");
+
+  } catch (PDOException $ex) {
+    echo $ex->getMessage();
+}
+
+}
+
+?>
