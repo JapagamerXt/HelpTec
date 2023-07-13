53e1dd9cce4f5df47fb2776d98e1e23cc0a1ba15
kaiozumba93 <137240878+kaiozumba93@users.noreply.github.com>
2023-07-13 16:44:24 -0300

    Add files via upload

--- apr.php
+++ apr.php
@@ -26,10 +26,21 @@
         </div>
         <div class="row mt-1" id="per02">  
         <div class="col-sm-12 text-center">
-            <h2>Risco</h2>
+            <h2>Grau de Risco</h2>
           </div> 
             <div class="col-sm-12">
-                <input type="text" name="risco_apr" id="risco_apr" class="form-control" placeholder="Risco">
+                 <input type="radio" id="resp_per02A"   name="grau_risco_apr" value="sim" class="form-check-input">
+          <label class="form-check-label" for="resp_per13A">
+           Sim
+          </label>
+          <input type="radio" id="resp_per02B" name="resp_per02" value="Não" class="form-check-input">
+          <label class="form-check-label" for="resp_per13B">
+            Não
+          </label>
+          <input type="radio" id="resp_per02C"  name="resp_per02" value="N/A" class="form-check-input">
+          <label class="form-check-label" for="resp_per13C">
+            N/A
+          </label>
             </div>
             <div class="col-sm-12 text-center mt-1">
             <button name="btoCadastrar" id="btAnterior" class="btn btn-success" onclick="Esconder()" >Anterior</button>
@@ -60,19 +71,43 @@
             <button name="btoCadastrar" id="btoProximo" class="btn btn-success" onclick="IrPergunta5()" >Próximo</button>
             </div>
         </div>
-        <div class="row mt-1" id="per05">
+       <div class="row mt-1" id="per05">
         <div class="col-sm-12 text-center">
-            <h2>Serviços</h2>
+            <h2>Atividades</h2>
           </div>
             <div class="col-sm-12">
-                <textarea name="servico_apr" id="servico_apr" cols="30" rows="5" placeholder="Serviços a Executar" class="form-control"></textarea>
+                <textarea name="atividades_apr" id="atividades_apr" cols="30" rows="5" placeholder="Serviços a Executar" class="form-control"></textarea>
             </div>
             <div class="col-sm-12 text-center mt-1">
             <button name="btoCadastrar" id="btAnterior" class="btn btn-success" onclick="IrPergunta4()" >Anterior</button>
             <button name="btoCadastrar" id="btoProximo" class="btn btn-success" onclick="IrPergunta6()" >Próximo</button>
             </div>
-        </div>   
-        <div class="row mt-1" id="per06">
+       </div>
+		<div class="row mt-1" id="per06">
+				<div class="col-sm-12 text-center">
+            <h2>Riscos</h2>
+				</div>
+            <div class="col-sm-12">
+                <textarea name="risco_apr" id="risco_apr" cols="30" rows="5" placeholder="Riscos(O que pode dar errado)" class="form-control"></textarea>
+            </div>
+           <div class="col-sm-12 text-center mt-1">
+            <button name="btoCadastrar" id="btAnterior" class="btn btn-success" onclick="IrPergunta5()" >Anterior</button>
+            <button name="btoCadastrar" id="btoProximo" class="btn btn-success" onclick="IrPergunta7()" >Próximo</button>
+           </div>
+        </div>
+         <div class="row mt-1" id="per07">
+				<div class="col-sm-12 text-center">
+                  <h2>Prevenção para possivel perdas</h2>
+				</div>
+            <div class="col-sm-12">
+                <textarea name="prev_apr" id="prev_apr" cols="30" rows="5" placeholder="Prevenção para possivel perdas" class="form-control"></textarea>
+            </div>
+           <div class="col-sm-12 text-center mt-1">
+            <button name="btoCadastrar" id="btAnterior" class="btn btn-success" onclick="IrPergunta6()" >Anterior</button>
+            <button name="btoCadastrar" id="btoProximo" class="btn btn-success" onclick="IrPergunta8()" >Próximo</button>
+           </div>
+        </div>  		
+        <div class="row mt-1" id="per08">
         <div class="col-sm-12 text-center">
             <h2>Observações</h2>
           </div>
@@ -80,12 +115,12 @@
                 <textarea name="obs_apr" id="obs_apr" cols="30" rows="5" placeholder="Observações da Apr" class="form-control"></textarea>
             </div>
             <div class="col-sm-12 text-center">
-            <button name="btoCadastrar" id="btAnterior" class="btn btn-success" onclick="IrPergunta5()" >Anterior</button>
-            <button name="btoCadastrar" id="btoProximo" class="btn btn-success" onclick="IrPergunta7()" >Próximo</button>
+            <button name="btoCadastrar" id="btAnterior" class="btn btn-success" onclick="IrPergunta7()" >Anterior</button>
+            <button name="btoCadastrar" id="btoProximo" class="btn btn-success" onclick="IrPergunta9()" >Próximo</button>
             </div>
         </div>  
        </div>   
-    <div class="row" id="per07">
+    <div class="row" id="per09">
           <div class="col-sm-12 text-center mt-1">
             <h2>Status</h2>
           </div>
@@ -97,7 +132,7 @@
                 </select>
         </div>
         <div class="col-sm-12 text-center">
-            <button name="btoCadastrar" id="btAnterior" class="btn btn-success" onclick="IrPergunta6()" >Anterior</button>
+            <button name="btoCadastrar" id="btAnterior" class="btn btn-success" onclick="IrPergunta8()" >Anterior</button>
             <button name="btoCadastrar" id="btoProximo" class="btn btn-success" onclick="Cadastrar()" >Finalizar</button>
             </div>
     </div> 
@@ -111,6 +146,8 @@
   let p5 = document.getElementById("per05");
   let p6 = document.getElementById("per06");
   let p7 = document.getElementById("per07");
+  let p8 = document.getElementById("per08");
+  let p9 = document.getElementById("per09");
 
   function Esconder()
   {
@@ -121,14 +158,17 @@
      p5.style.display = "none";
      p6.style.display = "none";
      p7.style.display = "none";
+	 p8.style.display = "none";
+	 p9.style.display = "none";
   }
   function IrPergunta2()
   {
-    let text = document.getElementById("setor_apr");
-    if(text.value == ''){
+     let text1 = document.getElementById("resp_per02A");
+    let text2 = document.getElementById("resp_per02B");
+    let text3 = document.getElementById("resp_per02C");
+    if(text1.checked == false && text2.checked == false && text3.checked == false){
       alert("Por favor, preencha os campos obrigatórios");
     }else{
-  
     p1.style.display = "none";
     p2.style.display = "block";
     p3.style.display = "none";
@@ -136,11 +176,13 @@
     p5.style.display = "none";
     p6.style.display = "none";
     p7.style.display = "none";
+	 p8.style.display = "none";
+	 p9.style.display = "none";
     }
 }
 function IrPergunta3()
   {
-    let text = document.getElementById("risco_apr");
+    let text = document.getElementById("garu_risco_apr");
     if(text.value == ''){
       alert("Por favor, preencha os campos obrigatórios");
     }else{
@@ -151,6 +193,8 @@ function IrPergunta3()
     p5.style.display = "none";
     p6.style.display = "none";
     p7.style.display = "none";
+	 p8.style.display = "none";
+	 p9.style.display = "none";
     }
 }  
 function IrPergunta4()
@@ -166,6 +210,8 @@ function IrPergunta4()
     p5.style.display = "none";
     p6.style.display = "none";
     p7.style.display = "none";
+	 p8.style.display = "none";
+	 p9.style.display = "none";
     }
 } 
 function IrPergunta5()
@@ -181,11 +227,13 @@ function IrPergunta5()
     p5.style.display = "Block";
     p6.style.display = "none";
     p7.style.display = "none";
+	 p8.style.display = "none";
+	 p9.style.display = "none";
     }
 } 
 function IrPergunta6()
   {
-    let text = document.getElementById("servicos_apr");
+    let text = document.getElementById("atividades_apr");
     if(text.value == ''){
       alert("Por favor, preencha os campos obrigatórios");
     }else{
@@ -200,7 +248,7 @@ function IrPergunta6()
 }
 function IrPergunta7()
   {
-    let text = document.getElementById("obs_apr");
+    let text = document.getElementById("risco_apr");
     if(text.value == ''){
       alert("Por favor, preencha os campos obrigatórios");
     }else{
@@ -211,8 +259,46 @@ function IrPergunta7()
     p5.style.display = "none";
     p6.style.display = "none";
     p7.style.display = "Block";
+	 p8.style.display = "none";
+	 p9.style.display = "none";
     }
 }  
+function IrPergunta8()
+  {
+    let text = document.getElementById("prev_apr");
+    if(text.value == ''){
+      alert("Por favor, preencha os campos obrigatórios");
+    }else{
+    p1.style.display = "none";
+    p2.style.display = "none";
+    p3.style.display = "none";
+    p4.style.display = "none";
+    p5.style.display = "none";
+    p6.style.display = "none";
+    p7.style.display = "none";
+	 p8.style.display = "block";
+	 p9.style.display = "none";
+    }
+}  
+function IrPergunta9()
+  {
+    let text = document.getElementById("obs_apr");
+    if(text.value == ''){
+      alert("Por favor, preencha os campos obrigatórios");
+    }else{
+    p1.style.display = "none";
+    p2.style.display = "none";
+    p3.style.display = "none";
+    p4.style.display = "none";
+    p5.style.display = "none";
+    p6.style.display = "none";
+    p7.style.display = "none";
+	 p8.style.display = "none";
+	 p9.style.display = "block";
+    }
+}  
+
+
 </script>
 
 </body>
