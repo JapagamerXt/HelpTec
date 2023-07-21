42a15ef954dbc6838751abd70640d19422976edc
JapagamerXt <kayooohenrique@gmail.com>
2023-07-20 22:11:16 -0300

    Nr10 att

--- Projeto/Respostas_Nr10.php
+++ Projeto/Respostas_Nr10.php
@@ -1,6 +1,4 @@
-<?php 
-     include_once("conexao.php");
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,56 +8,65 @@
     <title>Respostas</title>
 </head>
 <body>
-    <?php 
-    
-        
-       $resp_Nr10 = "SELECT * FROM Nr_10 ";
-       if ($reso=mysqli_query($conn, $sql)) {
-        
-       };
-   
+    <div class="container">
+        <table class='table'>
+            <thead>
+                <tr>
+                    <th scope='col'>1</th>
+                    <th scope='col'>2</th>
+                    <th scope='col'>3</th>
+                    <th scope='col'>4</th>
+                    <th scope='col'>5</th>
+                    <th scope='col'>6</th>
+                    <th scope='col'>7</th>
+                    <th scope='col'>8</th>
+                    <th scope='col'>9</th>
+                    <th scope='col'>10</th>
+                    <th scope='col'>11</th>
+                    <th scope='col'>12</th>
+                    <th scope='col'>13</th>
+                    <th scope='col'>14</th>
+                    <th scope='col'>15</th>
+                </tr>
+            </thead>
 
-    ?>
+            <tbody>
+                <?php 
+                    include_once("conexao.php");
+                    try {
+                        $sql = $conn->query('select* from Nr_10 where Id_Nr10');
+                        if ($sql->rowCount()>=1) {
+                        foreach ($sql as $row) {
+                            echo "<tr>";
+                            echo "<th scope='row'>$row[0]</th>";
+                            echo " <th scope='row'>$row[1]</th>";
+                            echo "<th scope='row'>$row[2]</th>";
+                            echo "<th scope='row'>$row[3]</th>";
+                            echo "<th scope='row'>$row[4]</th>";
+                            echo "<th scope='row'>$row[5]</th>";
+                            echo " <th scope='row'>$row[6]</th>";
+                            echo " <th scope='row'>$row[7]</th>";
+                            echo "<th scope='row'>$row[8]</th>";
+                            echo "<th scope='row'>$row[9]</th>";
+                            echo " <th scope='row'>$row[10]</th>";
+                            echo " <th scope='row'>$row[11]</th>";
+                            echo " <th scope='row'>$row[12]</th>";
+                            echo " <th scope='row'>$row[13]</th>";
+                            echo " <th scope='row'>$row[14]</th>";
+                            echo "</tr>";
+                        }
+                        }
+                    } catch (\Throwable $th) {
+                        
+                    }
+            
 
-    <!-- <table class="table">
-    <thead>
-        <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
-        <th scope="col">Handle</th>
-        <th scope="col">Handle</th>
-        <th scope="col">Handle</th>
-        <th scope="col">Handle</th>
-        <th scope="col">Handle</th>
-        <th scope="col">Handle</th>
-        <th scope="col">Handle</th>
-        <th scope="col">Handle</th>
+                ?>
+            </tbody>
+        </table>     
 
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        </tr>
-        <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        </tr>
-        <tr>
-        <th scope="row">3</th>
-        <td colspan="2">Larry the Bird</td>
-        <td>@twitter</td>
-        </tr>
-    </tbody>
-    </table> -->
-    
+    </div>
+                   
     <script src="../js/bootstrap.js"></script>
     
 </body>
